29a34cd9fae2726ef4f785e867261eeef39991b5
BD-Macbookpro <bd-macbookpro@MacBook-Pro-khxng-BD-Macbookpro-6.local>
2026-06-24 11:13:04 +0700

    Add static bundle build pipeline

---
+++ src/data/tweaks.js
@@ -0,0 +1,9 @@
+(function () {
+  window.TWEAK_DEFAULTS = /*EDITMODE-BEGIN*/{
+    "accentColor": "#b8976a",
+    "navyColor": "#0f1e35",
+    "heroLayout": "dark",
+    "font": "Cormorant Garamond",
+    "language": "en"
+  }/*EDITMODE-END*/;
+})();
